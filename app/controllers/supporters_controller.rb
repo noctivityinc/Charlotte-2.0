6d51070df37b2605c997c03dadd729ea5369129b
Joshua Lippiner <jlippiner@joshua-lippiners-macbook-pro.local>
2009-11-30 22:38:10 -0500

    fixed mailing issue

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -1,24 +1,26 @@
 class SupportersController < PublicController
-   def index
-     @supporter = Supporter.new
-   end
+  def index
+    @supporter = Supporter.new
+  end
 
-   def new
-     @supporter = Supporter.new
-     render :index
-   end
+  def new
+    @supporter = Supporter.new
+    render :index
+  end
 
-   def create
-     @supporter = Supporter.new(params[:supporter])
-     if @supporter.save
-       flash[:notice] = "Thank you for supporting Charlotte 2.0"
-       redirect_to root_url
-     else
-       flash[:error] = "There was a problem saving your information.  Please see the form on the right."
-       render :action => 'index'
-     end
-   end
+  def create
+    @supporter = Supporter.new(params[:supporter])
+    if @supporter.save
+      flash[:notice] = "Thank you for supporting Charlotte 2.0"
+      Notify.deliver_thankyou(@supporter)
+      Notify.deliver_new_supporter(@supporter)
+      redirect_to root_url
+    else
+      flash[:error] = "There was a problem saving your information.  Please see the form on the right."
+      render :action => 'index'
+    end
+  end
 
-   private
+  private
 
 end
