80be1f6b24ffce842abee118a99c445b9483d2c0
Joshua Lippiner <jlippiner@joshua-lippiners-macbook-pro.local>
2010-01-13 10:01:43 -0500

    removed logos

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -15,7 +15,7 @@ class Admin::PostsController < AdminController
   
   def create
     @post = Post.new(params[:post])
-    if @post.save
+    if @post.save!
       flash[:notice] = "Successfully created post."
       redirect_to [:admin, @post]
     else
